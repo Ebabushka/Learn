47a74a53143ac89d0bca7163f53c26bed0da9931
Olejan <oadmirtiriev13@gmail.com>
2024-02-26 20:03:18 +0300

    поправил замечания

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,31 @@
-import java.util.Scanner;
-class MyFirstParasha {
-    public static void main (String [] args) {
-        double x, x1, x2, D;
-        Scanner s = new Scanner(System.in);
-        System.out.println("Введите значение переменной: a");
-        double a = s.nextInt();
-        System.out.println("Вы ввели "+a);
-        System.out.println("Введите значение переменной: b");
-        double b = s.nextInt();
-        System.out.println("Вы ввели: "+b);
-        System.out.println("Введите значение переменной: c");
-        double c = s.nextInt();
-        System.out.println("Вы ввели: "+c);
-        D = b*b - 4 * a * c;
-        if (a == 0) {
-            x = -c / b;
-            System.out.printf ("x = %s", x);
-        }
 
-        else if (D == 0) {
-            x = x1 = x2 = -b / (2 * a);
-            System.out.printf ("D = %s, \n x = %s", D, x);
-        }
-        else if (D > 0) {
-            x1 = (-b+Math.sqrt(D))/(2 * a);
-            x2 = (-b-Math.sqrt(D))/(2 * a);
-            System.out.printf ("D = %s, \nx1 = %s, \nx2 = %s", D, x1, x2);
-        }
-        else if (D < 0) {
-            System.out.println ("Нет действительных корней");
+import java.util.Scanner;
+
+class MyFirstParasha {
+    public static void main(String[] args) {
+        Double a = Double.parseDouble(args[0]);
+        Double b = Double.parseDouble(args[1]);
+        Double c = Double.parseDouble(args[2]);
+        System.out.println(String.format("(%s) * x^2 + (%s) * x + (%s) = 0", a, b, c));
+        Double D = b * b - 4 * a * c;
+        if (a == 0) {
+            Double x = -c / b;
+            System.out.println(String.format("x = %s", x));
+        } else {
+            if (D == 0) {
+                Double x = -b / (2 * a);
+                System.out.println(String.format("D = %s, \n x = %s", D, x));
+            } else {
+                if (D > 0) {
+                    Double x1 = (-b + Math.sqrt(D)) / (2 * a);
+                    Double x2 = (-b - Math.sqrt(D)) / (2 * a);
+                    System.out.printf(String.format("D = %s, \nx1 = %s, \nx2 = %s", D, x1, x2));
+                } else {
+                    if (D < 0) {
+                        System.out.println("Нет действительных корней");
+                    }
+                }
+            }
         }
     }
 }
