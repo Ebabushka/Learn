1d7311ca03e51957e19a2437321e16c1312853e6
Olejan <oadmirtiriev13@gmail.com>
2024-03-17 19:34:36 +0300

    поправил замечания

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
-import java.util.Scanner;
-
 class MyFirstParasha {
+    public static void task_1() {
+        System.out.println(String.format("Задание №1: \nВывод квадратов натуральных чисел."));
+        Integer i = 0;
+        Integer x = i + 1;
+        Integer n = 50;
+        while (x*x < n) {
+            System.out.println(String.format("Ответ: %s", (Math.pow(x, 2))));
+            x ++;
+        }
+    }
+    public static void task_2() {
+        System.out.println(String.format("Задание №2: \nКубы чисел от A до B."));
+        Integer b = 6;
+        for (Integer a = 0; a <= b; a ++) {
+            System.out.println(String.format("Ответ: %s", (Math.pow(a, 3))));
+        }
+    }
     public static void main(String[] args) {
+        task_1();
+        task_2();
     }
 }
