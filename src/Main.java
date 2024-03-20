58d046bff9e0d28c027b0bce9a5beed82a4791f7
Olejan <oadmirtiriev13@gmail.com>
2024-03-20 22:39:32 +0300

    поправил замечания

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,7 @@ class MyFirstParasha {
     public static Double task_3() {
         System.out.println(String.format("Задание №3: \nВозведение числа в степень."));
         Double res = 1.0;
-        Double a = 6.0;
+        Double a = -6.0;
         Integer n = 5;
         if (n.equals(0)) {
             res.equals(1);
@@ -51,10 +51,39 @@ class MyFirstParasha {
         }
     }
 
+    public static Integer task_5() {
+        System.out.println(String.format("Задание №5: \nВычисление факториала числа."));
+        Integer n = 0;
+        Integer a = 4;
+        for (Integer i = 1; i <= a; i++) {
+            n *= i;
+        }
+        System.out.println(String.format("Ответ: %s", n));
+        return n;
+    }
+
+    public static void task_6() {
+        System.out.println(String.format("Задание №6: \nЧисла Фибоначчи."));
+        Integer a = 0;
+        Integer b = 1;
+        System.out.print(a + " " + b + " ");
+        for(Integer i = 3; i <= 15; i++) {
+            Integer c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
+    }
+    public static void task_7() {
+    Integer I
+}
     public static void main(String[] args) {
         task_1();
         task_2();
         task_3();
         task_4();
+        task_5();
+        task_6();
+        task_7();
     }
 }
