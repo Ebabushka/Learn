1f6534faed728235beb3aba79dbccb46a2ee6778
Olejan <oadmirtiriev13@gmail.com>
2024-06-18 22:48:24 +0300

    ready

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Random;
 
 class MyFirstParasha {
     public static void main(String[] args) {
+        int length = 10;
+        int[] massive = new int[length];
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            massive[i] = random.nextInt(201) - 100;
+        }
+        System.out.println(Arrays.toString(massive));
+        int min, max;
+        min = max = massive[0];
+        for (int i = 1; i < length; i++) {
+            if (massive[i] < min) {
+                min = massive[i];
+            }
+            if (massive[i] > max) {
+                max = massive[i];
+            }
+        }
+        System.out.println(min + " " + max);
     }
 }
