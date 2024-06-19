5398f45b80c2e944fd85a087de7b74314e9068ea
Olejan <oadmirtiriev13@gmail.com>
2024-06-19 22:27:49 +0300

    ready

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,31 @@ import java.util.Arrays;
 import java.util.Random;
 
 class MyFirstParasha {
-    public static void main(String[] args) {
-        int length = 10;
+    public static int[] createMassive(int length, int minNumber, int maxNumber) {
         int[] massive = new int[length];
-        int minNumber = 10;
-        int maxNumber = 40;
         Random random = new Random();
         for (int i = 0; i < length; i++) {
-            massive[i] = random.nextInt(maxNumber-minNumber)+minNumber;
+            massive[i] = random.nextInt(maxNumber - minNumber) + minNumber;
         }
-        System.out.println(Arrays.toString(massive));
+        return massive;
     }
+
+    public static int[] printMassive(int[] massive) {
+        for (int i = 0; i < massive.length; i++) {
+            System.out.print(massive[i] + " ");
+        }
+        return massive;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(String.format("Задание №1: Нахождение суммы всех элементов массива:"));
+        int sumResult = 0;
+        int[] massive = createMassive(10, 5, 20);
+        printMassive(massive);
+        for (int i = 0; i < massive.length; i++) {
+            sumResult = sumResult + massive[i];
+        }
+        System.out.println(String.format("\nОтвет: %s.", sumResult));
+    }
+
 }
