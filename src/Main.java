35ec8961ecb4ea53062b01438948dcfb4928561d
Olejan <oadmirtiriev13@gmail.com>
2024-06-19 20:41:31 +0300

    ready

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,12 @@ class MyFirstParasha {
     public static void main(String[] args) {
         int length = 10;
         int[] massive = new int[length];
+        int minNumber = 10;
+        int maxNumber = 40;
         Random random = new Random();
         for (int i = 0; i < length; i++) {
-            massive[i] = random.nextInt(201) - 100;
+            massive[i] = random.nextInt(maxNumber-minNumber)+minNumber;
         }
         System.out.println(Arrays.toString(massive));
-        int min, max;
-        min = max = massive[0];
-        for (int i = 1; i < length; i++) {
-            if (massive[i] < min) {
-                min = massive[i];
-            }
-            if (massive[i] > max) {
-                max = massive[i];
-            }
-        }
-        int [] massive_2 = {length, min, max};
-        System.out.println(Arrays.toString(massive_2));
     }
 }
