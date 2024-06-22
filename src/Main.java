93c63bde8c4e056c7b54df90762e5db044692def
Olejan <oadmirtiriev13@gmail.com>
2024-06-22 15:47:43 +0300

    rea

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,11 @@ class MyFirstParasha {
     }
 
     public static String printMassive(int[] massive) {
-        String stringMassive = "";
+        StringBuilder stringMassive = new StringBuilder();
         for (int i = 0; i < massive.length; i++) {
-            stringMassive = stringMassive + massive[i] + " ";
+            stringMassive.append(massive[i]).append(" ");
         }
-        return stringMassive;
+        return stringMassive.toString();
     }
 
     public static void task_1() {
@@ -31,15 +31,42 @@ class MyFirstParasha {
 
     public static void task_2() {
         System.out.println(String.format("Задание №2: Нахождение среднего арифметического значения четных элементов массива:"));
-        int arithmeticMean = 0;
         int[] massive = createMassive(10, 5, 20);
         System.out.print(printMassive(massive));
-        Integer firstNumberDigit = 0;
-        Integer secondNumberDigit = 0;
+        int counter = 0;
+        double sumEvenNumber = 0;
+        for (int i = 0; i < massive.length; i++) {
+            if (massive[i] % 2 == 0) {
+                counter = counter + 1;
+                sumEvenNumber = sumEvenNumber + massive[i];
+            }
+        }
+        double arithmeticMean = sumEvenNumber / counter;
+        System.out.print(String.format("\nОтвет: %s\n", arithmeticMean));
+    }
+
+    public static void task_3() {
+        System.out.println(String.format("Задание №3: Нахождение номеров (индексов) простых чисел элементов массива:"));
+        int[] massive = createMassive(10, 5, 20);
+        System.out.print(printMassive(massive));
+        StringBuilder index = new StringBuilder();
+        for (int i = 0; i < massive.length; i++) {
+            boolean isSimple = true;
+            for (int j = 2; j <= Math.sqrt(massive[i]); j++) {
+                if (massive[i] % j == 0) {
+                    isSimple = false;
+                }
+            }
+            if (isSimple) {
+                index.append(i).append(" ");
+            }
+        }
+        System.out.println(String.format("\nОтвет: %s", index));
     }
 
     public static void main(String[] args) {
         task_1();
         task_2();
+        task_3();
     }
 }
